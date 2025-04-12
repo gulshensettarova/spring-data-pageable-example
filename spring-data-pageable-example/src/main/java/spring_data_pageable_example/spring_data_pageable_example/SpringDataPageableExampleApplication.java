5a0340de3ee4ad0c9e346b421dc2bfcfe6456cb3
gulshensettarova <gulshensettarova@gmail.com>
2025-04-13 00:04:19 +0400

    Initial project

---
+++ spring-data-pageable-example/src/main/java/spring_data_pageable_example/spring_data_pageable_example/SpringDataPageableExampleApplication.java
@@ -0,0 +1,13 @@
+package spring_data_pageable_example.spring_data_pageable_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataPageableExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataPageableExampleApplication.class, args);
+	}
+
+}
---
+++ spring-data-pageable-example/src/test/java/spring_data_pageable_example/spring_data_pageable_example/SpringDataPageableExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_data_pageable_example.spring_data_pageable_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataPageableExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
